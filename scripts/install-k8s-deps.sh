9698db8a220511ee547efdd060e67da81df7a62c
Nawbc <wanghan9423@outlook.com>
2022-01-13 16:37:17 +0800

    fix:version

--- scripts/install-k8s-deps.sh
+++ scripts/install-k8s-deps.sh
@@ -1,1 +1,8 @@
-microk8s enable dns helm3 dashboard ingress
+microk8s enable dns helm3 dashboard ingress storage
+microk8s ctr images pull registry.aliyuncs.com/google_containers/pause:3.1
+microk8s ctr images tag registry.aliyuncs.com/google_containers/pause:3.1 k8s.gcr.io/pause:3.1
+microk8s ctr images pull registry.aliyuncs.com/google_containers/metrics-server:v0.5.0
+microk8s ctr images tag registry.aliyuncs.com/google_containers/metrics-server:v0.5.0 k8s.gcr.io/metrics-server/metrics-server:v0.5.0
+microk8s ctr images pull docker.io/liangjw/ingress-nginx-controller:v1.0.0-alpha.2
+microk8s ctr images tag docker.io/liangjw/ingress-nginx-controller:v1.0.0-alpha.2 k8s.gcr.io/ingress-nginx/controller:v1.0.0-alpha.2
+
---
+++ scripts/start-kuboard.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+sudo docker run -d \
+  --restart=unless-stopped \
+  --name=kuboard \
+  -p 80:80/tcp \
+  -p 10081:10081/tcp \
+  -e KUBOARD_ENDPOINT="http://192.168.201.107:80" \
+  -e KUBOARD_AGENT_SERVER_TCP_PORT="10081" \
+  -v /root/kuboard-data:/data \
+  swr.cn-east-2.myhuaweicloud.com/kuboard/kuboard:v3
