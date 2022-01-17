470b7df20c9d2a3a68805ede3b9c0abfb7c960a2
Nawbc <wanghan9423@outlook.com>
2022-01-17 11:16:43 +0800

    fix:version1

--- scripts/install-k8s-deps.sh
+++
@@ -1,9 +0,0 @@
-microk8s enable dns helm3 dashboard ingress storage
-microk8s ctr images pull registry.aliyuncs.com/google_containers/pause:3.1
-microk8s ctr images tag registry.aliyuncs.com/google_containers/pause:3.1 k8s.gcr.io/pause:3.1
-microk8s ctr images pull registry.aliyuncs.com/google_containers/metrics-server:v0.5.0
-microk8s ctr images tag registry.aliyuncs.com/google_containers/metrics-server:v0.5.0 k8s.gcr.io/metrics-server/metrics-server:v0.5.0
-microk8s ctr images pull docker.io/liangjw/ingress-nginx-controller:v1.0.0-alpha.2
-microk8s ctr images tag docker.io/liangjw/ingress-nginx-controller:v1.0.0-alpha.2 k8s.gcr.io/ingress-nginx/controller:v1.0.0-alpha.2
-
-helm install minio/minio:RELEASE.2021-02-14T04-01-33Z --namespace minio --create-namespace --generate-name
--- scripts/start-kuboard.sh
+++ scripts/start-kuboard.sh
@@ -1,11 +1,27 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-sudo docker run -d \
+# set +e
+# docker kill $(docker container ls -f "name=kuboard" -q) && docker rm $(docker container ls -a -f "name=kuboard" -q)
+# set -e
+
+IP=$(microk8s kubectl get node -o json | jq '.items[].status.addresses[] | select(.type=="InternalIP") | .address' | xargs)
+
+echo $IP
+
+docker run -d \
   --restart=unless-stopped \
   --name=kuboard \
-  -p 80:80/tcp \
+  -p 10088:80/tcp \
   -p 10081:10081/tcp \
-  -e KUBOARD_ENDPOINT="http://192.168.201.107:80" \
+  -e KUBOARD_ENDPOINT="http://$IP:10088" \
   -e KUBOARD_AGENT_SERVER_TCP_PORT="10081" \
   -v /root/kuboard-data:/data \
   swr.cn-east-2.myhuaweicloud.com/kuboard/kuboard:v3
+
+# sudo docker run -d \
+#   --restart=unless-stopped \
+#   --name=kuboard-spray \
+#   -p 10089:80/tcp \
+#   -v /var/run/docker.sock:/var/run/docker.sock \
+#   -v ~/kuboard-spray-data:/data \
+#   swr.cn-east-2.myhuaweicloud.com/kuboard/kuboard-spray:latest-amd64
