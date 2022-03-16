bfb08b65ddff83155d334b4081e2cd44a6e56885
Nawbc <wanghan9423@outlook.com>
2022-03-16 18:09:27 +0800

    save

--- first_party/nacos/src/index.ts
+++ first_party/nacos/src/index.ts
@@ -1,3 +1,4 @@
 export { NacosConfiguration as Configuration } from './configuration';
 export * from './nacos.config';
 export * from './nacos.naming';
+export * from './nacos.interface';
---
+++ first_party/nacos/src/nacos.interface.ts
@@ -0,0 +1,1 @@
+export { ClientOptions as NacosConfigClientOptions } from 'nacos';
