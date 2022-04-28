6ffe5b7d5f9fd4e086e57e7eccea7646f40906af
Nawbc <wanghan9423@outlook.com>
2022-04-28 14:13:28 +0800

    tmp

--- apps/app/components/Footer.tsx
+++ apps/app/components/Footer.tsx
@@ -1,10 +1,6 @@
 import { Stack, Button, Typography, GlobalStyles } from '@mui/material';
 import Link from 'next/link';
 
-const alinkGlobalStyle = (
-  <GlobalStyles styles={{ a: { textDecoration: 'none' } }} />
-);
-
 export const Footer = function (props) {
   return (
     <footer>
@@ -14,12 +10,9 @@ export const Footer = function (props) {
           <Button>Two</Button>
           <Button>Three</Button>
         </Stack>
-        <Stack direction="row" spacing={2}>
-          {alinkGlobalStyle}
+        <Stack direction="row" justifyContent="center" spacing={2}>
           <Link href="/">皖ICP备20002736号-2</Link>
-          <Typography>
-            Copyright© {new Date().getFullYear()} Wang Han(Nawbc)
-          </Typography>
+          <Typography>Copyright© {new Date().getFullYear()} WangHan</Typography>
         </Stack>
       </Stack>
     </footer>
--- apps/app/pages/index.tsx
+++ apps/app/pages/index.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable  react/no-unescaped-entities */
 import type { NextPage } from 'next';
 import { TextParticle } from 'city-night';
 import { useWindowSize } from 'react-use';
-import { isDesktop, isMobile } from 'react-device-detect';
+import { isMobile } from 'react-device-detect';
 import styled from '@mui/system/styled';
 import Button from '@mui/material/Button';
 import Toolbar from '@mui/material/Toolbar';
@@ -9,6 +10,23 @@ import AppBar from '@mui/material/AppBar';
 import { Footer, NavHeader } from 'components';
 import { useTheme } from '@mui/material/styles';
 import { IconFileCode } from '@tabler/icons';
+import {
+  Animator,
+  ScrollContainer,
+  ScrollPage,
+  batch,
+  Fade,
+  FadeIn,
+  Move,
+  MoveIn,
+  MoveOut,
+  Sticky,
+  StickyIn,
+  ZoomIn,
+} from 'react-scroll-motion';
+
+const ZoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+const FadeUp = batch(Fade(), Move(), Sticky());
 
 const TextParticleWrapper = styled('div')(({ theme }) => ({
   width: '100%',
@@ -58,11 +76,7 @@ const Home: NextPage = () => {
   const theme = useTheme();
   const { width } = useWindowSize();
 
-  // const getTextParticleWidth = function () {};
-
   const isMobileDevice = width < theme.breakpoints.values.xl || isMobile;
-
-  console.log(theme.breakpoints.values.xl, width, isMobile, isDesktop);
 
   return (
     <>
@@ -79,30 +93,74 @@ const Home: NextPage = () => {
         </AppBar>
       </header>
 
-      <TextParticleWrapper>
-        <TextParticle
-          text="letscollab"
-          resolution={5}
-          boxWidth={isMobileDevice ? 1800 : width}
-          boxHeight={400}
-          size={300}
-        />
-        {/* <NavButtonWrapper> */}
-        <CustomButton
-          size="large"
-          variant="contained"
-          endIcon={<IconFileCode />}
-          sx={{
-            color: '#fff',
-            boxShadow: 'unset',
-            background: '#242424',
-          }}
-        >
-          开始
-        </CustomButton>
-        {/* </NavButtonWrapper> */}
-      </TextParticleWrapper>
+      <ScrollContainer>
+        <ScrollPage page={0}>
+          <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200))}>
+            <TextParticleWrapper>
+              <TextParticle
+                text="letscollab"
+                resolution={5}
+                boxWidth={isMobileDevice ? 1800 : width}
+                boxHeight={400}
+                size={300}
+              />
 
+              <CustomButton
+                size="large"
+                variant="contained"
+                endIcon={<IconFileCode />}
+                sx={{
+                  color: '#fff',
+                  boxShadow: 'unset',
+                  background: '#242424',
+                }}
+              >
+                开始
+              </CustomButton>
+            </TextParticleWrapper>
+          </Animator>
+        </ScrollPage>
+        <ScrollPage page={1}>
+          <Animator animation={ZoomInScrollOut}>
+            <span style={{ fontSize: '40px' }}>I'm FadeUpScrollOut ✨</span>
+          </Animator>
+        </ScrollPage>
+        <ScrollPage page={2}>
+          <Animator animation={FadeUp}>
+            <span style={{ fontSize: '40px' }}>I'm FadeUp ⛅️</span>
+          </Animator>
+        </ScrollPage>
+        <ScrollPage page={3}>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100%',
+            }}
+          >
+            <span style={{ fontSize: '40px' }}>
+              <Animator animation={MoveIn(-1000, 0)}>Hello Guys 👋🏻</Animator>
+              <Animator animation={MoveIn(1000, 0)}>
+                Nice to meet you 🙋🏻‍♀️
+              </Animator>
+              - I'm Seonghyeok -
+              <Animator animation={MoveOut(1000, 0)}>Good bye ✋🏻</Animator>
+              <Animator animation={MoveOut(-1000, 0)}>See you 💛</Animator>
+            </span>
+          </div>
+        </ScrollPage>
+        <ScrollPage page={4}>
+          <Animator animation={batch(Fade(), Sticky())}>
+            <span style={{ fontSize: '40px' }}>Done</span>
+            <br />
+            <span style={{ fontSize: '30px' }}>
+              There's FadeAnimation, MoveAnimation, StickyAnimation,
+              ZoomAnimation
+            </span>
+          </Animator>
+        </ScrollPage>
+      </ScrollContainer>
       <Footer />
     </>
   );
