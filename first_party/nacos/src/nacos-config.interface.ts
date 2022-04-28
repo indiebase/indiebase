9e55b58935402f121d046e37bbe9710438c6c3b7
Nawbc <wanghan9423@outlook.com>
2022-04-28 23:28:48 +0800

    tmp

---
+++ first_party/nacos/src/nacos-config.interface.ts
@@ -0,0 +1,23 @@
+import { Type, DynamicModule, ForwardReference } from '@nestjs/common';
+import { ClientOptions } from 'nacos';
+
+export interface DataParser {
+  (data: string | string[], ...args: any[]): any;
+}
+export interface NacosConfigClientOptions extends ClientOptions {
+  dataParser?: DataParser;
+  [key: string]: any;
+}
+
+export interface NacosConfigClientAsyncOptions<T = NacosConfigClientOptions> {
+  /**
+   * Factory function that returns an instance of the provider to be injected.
+   */
+  useFactory: (...args: any[]) => T;
+
+  inject?: any[];
+
+  imports?: Array<
+    Type<any> | DynamicModule | Promise<DynamicModule> | ForwardReference
+  >;
+}
