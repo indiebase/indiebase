a7023173ba1b033d00b4da052a19dfa9eb71f9b1
Nawbc <wanghan9423@outlook.com>
2022-05-18 17:17:54 +0800

    move common package to first_party

--- packages/auth/src/auth/auth.service.ts
+++ packages/auth/src/auth/auth.service.ts
@@ -37,16 +37,7 @@ export class AuthService implements OnModuleInit {
   async validateUser(info: AvailableUserInfo): Promise<any> {}
 
   async signup(user: SignupDto): Promise<any> {
-    const { captcha, username } = user;
-    const key = Captcha.getSignupCaptchaToken(captcha, username);
-    const c = await this.redis.get(key);
-
-    if (c) {
-      await this.redis.del(key);
-    } else {
-      throw new BadRequestException({ message: '验证码错误或失效' });
-    }
-
+    const { captcha, account } = user;
     const result = await lastValueFrom(
       this.userClient.send({ cmd: 'signup' }, user).pipe(timeout(4000)),
     );
