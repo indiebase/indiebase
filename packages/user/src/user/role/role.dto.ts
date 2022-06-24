fa26095600a1a993d73a56154b99b4120178e606
Nawbc <wanghan9423@outlook.com>
2022-06-24 16:43:54 +0800

    daily

--- packages/user/src/user/role/role.dto.ts
+++ packages/user/src/user/role/role.dto.ts
@@ -1,19 +1,47 @@
+import { RoleStatus } from './role.entity';
 import { ApiPropertyOptional, ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
 
 export class CreateRoleDto {
   @ApiProperty({
-    default: 'admin',
+    default: 'member',
   })
   @IsNotEmpty({
     message: `Role name can't be empty`,
   })
-  name?: string;
+  name: string;
 
   @ApiPropertyOptional({
-    description: '角色描述',
-    default: '管理员',
+    description: 'Role description',
+    default: 'member',
   })
   @IsOptional()
   description?: string;
+}
+export class UpdateRoleDto {
+  @IsNumber()
+  id: number;
+
+  @ApiPropertyOptional({
+    default: 'admin',
+  })
+  name?: string;
+
+  @ApiPropertyOptional({
+    default: 'member',
+  })
+  @IsOptional()
+  description?: string;
+
+  @ApiProperty({
+    description: 'Role  status',
+    enum: RoleStatus,
+    default: RoleStatus.active,
+  })
+  @IsOptional()
+  status?: RoleStatus;
+}
+export class DeleteRoleDto {
+  @IsNumber()
+  id: number;
 }
