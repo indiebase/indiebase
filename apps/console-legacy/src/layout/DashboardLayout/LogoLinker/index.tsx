58eefb9040c04ddf2fb0f46fe4ff7dd685ea6036
Nawbc <wanghan9423@outlook.com>
2022-07-05 13:33:06 +0800

    daliy

--- apps/console-legacy/src/layout/DashboardLayout/LogoLinker/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from 'react-router-dom';
-import { ButtonBase } from '@mui/material';
-import Logo from '@/components/Logo';
-
-export const LogoLinker = () => (
-  <ButtonBase disableRipple component={Link} to="/home">
-    <Logo width={180} height={34} viewBox="0 0 1246 225" />
-  </ButtonBase>
-);
