ebbc830239128dd36dbebab85c7349b269bcec37
Nawbc <wanghan9423@outlook.com>
2022-08-24 05:28:05 +0000

    s

--- scripts/github_host.sh
+++ scripts/github_host.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # For chinese users
-/bin/sed -i "/# GitHub520 Host Start/Q" /etc/hosts && /bin/curl https://raw.hellogithub.com/hosts >>/etc/hosts
+/bin/sed -i "/# Hosts Start/Q" /etc/hosts && /bin/curl https://raw.githubusercontent.com/JohyC/Hosts/main/GithubHosts.txt >>/etc/hosts
