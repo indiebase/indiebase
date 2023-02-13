3dab97be278d2c84144ba781cf6d221079373ea2
Nawbc <wanghan9423@outlook.com>
2023-02-13 19:07:54 +0800

    s

--- apps/console/src/pages/Org/Settings/GeneralPage/index.tsx
+++ apps/console/src/pages/Org/Settings/GeneralPage/index.tsx
@@ -1,27 +1,11 @@
 import { Table } from '@letscollab-react/table';
+import { Grid, useMantineTheme } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import { createColumnHelper } from '@tanstack/react-table';
-import { Suspense } from 'react';
-
-// const countAtom = atom(1);
-// const derivedAtom = atom(
-//   (get) => get(countAtom),
-//   (get, set, action: any) => {
-//     if (action === 'a') {
-//       set(countAtom, 10);
-//     } else if (action === 'b') {
-//       set(countAtom, (c) => c + 1);
-//     }
-
-//     const a = get(countAtom);
-//     const b = get(countAtom);
-//     console.log()
-//     console.log(get(countAtom));
-//     console.log(a, '===============');
-//   },
-// );
+import { Suspense, useState } from 'react';
 
 type Person = {
-  firstName: string;
+  role: string;
   lastName: string;
   age: number;
   visits: number;
@@ -29,46 +13,76 @@ type Person = {
   progress: number;
 };
 
+const defaultData: any[] = [
+  {
+    role: 'tanner',
+    lastName: 'linsley',
+    age: 24,
+    visits: 100,
+    status: 'In Relationship',
+    progress: 50,
+  },
+  {
+    role: 'tandy',
+    lastName: 'miller',
+    age: 40,
+    visits: 40,
+    status: 'Single',
+    progress: 80,
+  },
+  {
+    role: 'joe',
+    lastName: 'dirte',
+    age: 45,
+    visits: 20,
+    status: 'Complicated',
+    progress: 10,
+  },
+];
+
 const columnHelper = createColumnHelper<Person>();
 
 const columns = [
-  columnHelper.accessor('firstName', {
-    cell: (info) => info.getValue(),
-    footer: (info) => info.column.id,
-    header(value) {
-      console.log(value);
-      return <span>FirstName</span>;
-    },
+  columnHelper.accessor('role', {
+    enableSorting: false,
+    header: 'Role',
+    enableColumnFilter: true,
   }),
   columnHelper.accessor((row) => row.lastName, {
+    enableSorting: false,
     id: 'lastName',
-    cell: (info) => <i>{info.getValue()}</i>,
-    footer: (info) => info.column.id,
   }),
   columnHelper.accessor('age', {
     header: () => 'Age',
-    cell: (info) => info.renderValue(),
-    footer: (info) => info.column.id,
   }),
   columnHelper.accessor('visits', {
     header: () => <span>Visits</span>,
-    footer: (info) => info.column.id,
   }),
   columnHelper.accessor('status', {
+    enableSorting: false,
     header: 'Status',
-    footer: (info) => info.column.id,
   }),
   columnHelper.accessor('progress', {
     header: 'Profile Progress',
-
-    footer: (info) => info.column.id,
   }),
 ];
 
 export const GeneralPage = function (props) {
+  const [data, setData] = useState(() => [...defaultData]);
+
+  const theme = useMantineTheme();
+  const matches = useMediaQuery(`(max-width: ${theme.breakpoints.sm}px)`);
+
   return (
     <Suspense>
-      <Table<Person> columns={columns} data={[]}></Table>
+      <Grid gutter="xs">
+        <Grid.Col span={matches ? 12 : 7}>
+          <Table<Person> title="Role" columns={columns} data={data}></Table>
+        </Grid.Col>
+        {/* <Grid.Col span={5}>
+          <MemberTable />
+        </Grid.Col> */}
+      </Grid>
     </Suspense>
   );
 };
