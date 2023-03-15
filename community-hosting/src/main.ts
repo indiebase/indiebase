1fcd3217f4f6174163292b0bd02cc7272e3757c2
Nawbc <wanghan9423@outlook.com>
2023-03-15 13:29:41 +0800

    s

--- community-hosting/src/main.ts
+++ community-hosting/src/main.ts
@@ -4,10 +4,9 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
-import { setupApiDoc } from './utils';
 import { AppModule } from './app.module';
-import fastifyHelmet from '@fastify/helmet';
-import { HttpExceptionFilter } from '@letscollab/server-shared';
+import fastifyHelmet, { FastifyHelmetOptions } from '@fastify/helmet';
+import { HttpExceptionFilter, kDevMode } from '@letscollab/server-shared';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { Logger, ValidationPipe, VersioningType } from '@nestjs/common';
 import { resolve } from 'path';
@@ -15,28 +14,26 @@ import { useContainer } from 'class-validator';
 import fastifyMultipart from '@fastify/multipart';
 import { i18nValidationErrorFactory } from 'nestjs-i18n';
 
-const isDevelopment = process.env.NODE_ENV === 'development';
-
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
     {
       bodyParser: true,
-      logger: isDevelopment ? ['verbose'] : ['error', 'warn'],
+      logger: kDevMode ? ['verbose'] : ['error', 'warn'],
     },
   );
 
   const logger = app.get(Logger);
 
   try {
-    const configService = app.get<ConfigService>(ConfigService);
+    const config = app.get<ConfigService>(ConfigService);
     const nestWinston = app.get(WINSTON_MODULE_NEST_PROVIDER);
 
     app.useGlobalPipes(
       new ValidationPipe({
         transform: true,
-        enableDebugMessages: isDevelopment,
+        enableDebugMessages: kDevMode,
         whitelist: true,
         forbidNonWhitelisted: true,
         exceptionFactory: i18nValidationErrorFactory,
@@ -48,7 +45,7 @@ async function bootstrap() {
       fallbackOnErrors: true,
     });
 
-    await app.register(fastifyHelmet, {
+    await app.register<FastifyHelmetOptions>(fastifyHelmet as any, {
       global: true,
       crossOriginOpenerPolicy: false,
       contentSecurityPolicy: false,
@@ -64,26 +61,25 @@ async function bootstrap() {
     });
 
     app.enableCors({
-      origin: configService.get('app.corsOrigin'),
+      origin: config.get('app.corsOrigin'),
       credentials: true,
     });
 
-    await setupApiDoc(app);
+    // await setupApiDoc(app);
 
-    app.register(fastifyMultipart);
+    app.register(fastifyMultipart as any);
 
     app.useStaticAssets({
       root: resolve(__dirname, '../public'),
     });
-    
+
     app.useLogger(nestWinston);
     app.useGlobalFilters(new HttpExceptionFilter(nestWinston));
 
-    await app.listen(
-      configService.get('app.port'),
-      configService.get('app.hostname'),
-    );
+    await app.listen(config.get('app.port'), config.get('app.hostname'));
   } catch (error) {
     logger.error(error);
   }
 }
+
+bootstrap();
