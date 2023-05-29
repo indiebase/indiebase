ae3adeabf769b92b79c6322dc514473ed653686c
Han <deskbtm@outlook.com>
2023-05-29 17:03:16 +0800

    s

---
+++ first_party/server-shared/src/constants.ts
@@ -0,0 +1,3 @@
+export const kDevMode = process.env.NODE_ENV === 'development';
+export const kTestMode = process.env.NODE_ENV === 'test';
+export const kReleaseMode = process.env.NODE_ENV === 'production';
