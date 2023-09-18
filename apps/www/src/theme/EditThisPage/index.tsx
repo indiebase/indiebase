179b93411c835c219d350f71ab09f907543abdbd
nawbc <wanghan9423@outlook.com>
2023-09-18 10:00:40 +0800

    t

--- apps/www/src/theme/EditThisPage/index.tsx
+++ apps/www/src/theme/EditThisPage/index.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import Translate from '@docusaurus/Translate';
-import {ThemeClassNames} from '@docusaurus/theme-common';
-import IconEdit from '@theme/Icon/Edit';
-import type {Props} from '@theme/EditThisPage';
+import { ThemeClassNames } from '@docusaurus/theme-common';
+import type { Props } from '@theme/EditThisPage';
+import { IconPencil } from '@tabler/icons-react';
+import { rem } from '@mantine/core';
 
-export default function EditThisPage({editUrl}: Props): JSX.Element {
+export default function EditThisPage({ editUrl }: Props): JSX.Element {
   return (
     <a
       href={editUrl}
       target="_blank"
       rel="noreferrer noopener"
-      className={ThemeClassNames.common.editThisPage}>
-      <IconEdit />
+      className={ThemeClassNames.common.editThisPage}
+      style={{ display: 'flex', alignItems: 'center' }}
+    >
       <Translate
         id="theme.common.editThisPage"
-        description="The link label to edit the current page">
+        description="The link label to edit the current page"
+      >
         Edit this page
       </Translate>
+      <IconPencil style={{ width: rem(18), marginLeft: rem(5) }} />
     </a>
   );
 }
