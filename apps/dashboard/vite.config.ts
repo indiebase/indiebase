e262de4548edcb8d5ec31b9614324cf163152a1a
nawbc <wanghan9423@outlook.com>
2023-11-27 11:17:18 +0800

    feat: Add millionjs

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -3,10 +3,16 @@ import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import { resolve } from 'node:path';
+import million from 'million/compiler';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA(), vanillaExtractPlugin()],
+  plugins: [
+    million.vite({ auto: true }),
+    react(),
+    VitePWA(),
+    vanillaExtractPlugin(),
+  ],
 
   resolve: {
     alias: {
