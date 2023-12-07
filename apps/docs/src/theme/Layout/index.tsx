b74359f9a808dcd9ab4b54257788c42f3600ec4e
Han <deskbtm@outlook.com>
2023-12-07 15:24:03 +0800

    tmp

--- apps/docs/src/theme/Layout/index.tsx
+++ apps/docs/src/theme/Layout/index.tsx
@@ -8,18 +8,15 @@ import {
   ThemeClassNames,
 } from '@docusaurus/theme-common';
 import { useKeyboardNavigation } from '@docusaurus/theme-common/internal';
-import { MantineProvider } from '@mantine/core';
 import AnnouncementBar from '@theme/AnnouncementBar';
 import ErrorPageContent from '@theme/ErrorPageContent';
 import Footer from '@theme/Footer';
 import LayoutProvider from '@theme/Layout/Provider';
 import Navbar from '@theme/Navbar';
 import SkipToContent from '@theme/SkipToContent';
-
+import type { Props } from '@theme/Layout';
 import styles from './styles.module.css';
 
-import type { Props } from '@theme/Layout';
-import { mantineTheme } from './mantine-theme';
 export default function Layout(props: Props): JSX.Element {
   const {
     children,
@@ -33,33 +30,29 @@ export default function Layout(props: Props): JSX.Element {
   useKeyboardNavigation();
 
   return (
-    <MantineProvider theme={mantineTheme} defaultColorScheme="light">
-      <LayoutProvider>
-        <PageMetadata title={title} description={description} />
+    <LayoutProvider>
+      <PageMetadata title={title} description={description} />
 
-        <SkipToContent />
+      <SkipToContent />
 
-        <AnnouncementBar />
+      <AnnouncementBar />
 
-        <Navbar />
+      <Navbar />
 
-        <div
-          id={SkipToContentFallbackId}
-          className={clsx(
-            ThemeClassNames.wrapper.main,
-            styles.mainWrapper,
-            wrapperClassName,
-          )}
-        >
-          <ErrorBoundary
-            fallback={(params) => <ErrorPageContent {...params} />}
-          >
-            {children}
-          </ErrorBoundary>
-        </div>
+      <div
+        id={SkipToContentFallbackId}
+        className={clsx(
+          ThemeClassNames.wrapper.main,
+          styles.mainWrapper,
+          wrapperClassName,
+        )}
+      >
+        <ErrorBoundary fallback={(params) => <ErrorPageContent {...params} />}>
+          {children}
+        </ErrorBoundary>
+      </div>
 
-        {!noFooter && <Footer />}
-      </LayoutProvider>
-    </MantineProvider>
+      {!noFooter && <Footer />}
+    </LayoutProvider>
   );
 }
