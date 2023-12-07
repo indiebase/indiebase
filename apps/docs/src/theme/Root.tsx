96ae4316ebdeafc4e6823d1881958bce68314d58
Han <deskbtm@outlook.com>
2023-12-07 18:30:50 +0800

    fix:  part of FOUC issues

--- apps/docs/src/theme/Root.tsx
+++ apps/docs/src/theme/Root.tsx
@@ -1,12 +1,16 @@
-import { MantineProvider } from '@mantine/core';
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import { mantineTheme } from './mantine-theme';
+import Head from '@docusaurus/Head';
 import React from 'react';
 
 // Default implementation, that you can customize
 export default function Root({ children }) {
   return (
-    <MantineProvider theme={mantineTheme} defaultColorScheme="light">
-      {children}
-    </MantineProvider>
+    <>
+      <ColorSchemeScript />
+      <MantineProvider theme={mantineTheme} defaultColorScheme="light">
+        {children}
+      </MantineProvider>
+    </>
   );
 }
