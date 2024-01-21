ac876a95904bb500576b4836450eb2f3a7c9f26d
Han <deskbtm@outlook.com>
2024-01-21 16:21:30 +0800

    feat(gadgets): add NOP

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/ban-types': 'error',
     'no-unused-vars': 'off',
